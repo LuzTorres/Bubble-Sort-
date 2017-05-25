549cbad765e7aee630f8164122ac79fca357108f
LuzTorres <torres.luz.21@hotmail.com>
2017-05-25 14:42:26 -0500

    Add files via upload

---
+++ bubble sort .cpp
@@ -0,0 +1,18 @@
+std::vector<char> v;
+vector Bubblesort(vector l){
+	for (int i = 0; i < l.size(); ++i)
+	{
+		for (int j = 0; j <l.size() count; ++i)
+		{
+		
+			{
+				if (l[j]> l[j+1])
+				{
+					int=aux=l[j];
+					l[j]=l[j+1];
+					l[j+1]=aux;
+				}
+			}
+		}
+	}
+}
